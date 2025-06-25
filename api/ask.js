c745ac4e11aebed316ddcf5077c01c7cb9731883
Revanth9984 <revanthreddytarimala@gmail.com>
2025-06-25 18:42:35 +0530

    CORS bux fix 2

--- api/ask.js
+++ api/ask.js
@@ -6,6 +6,10 @@ export default async function handler(req,res) {
    res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
+   if (req.method === "OPTIONS") {
+    return res.status(200).end(); // handle preflight
+  }
+  
     if(req.method!=="POST"){
         return res.status(405).json({error:"Only POST requests allowed"});
     }
